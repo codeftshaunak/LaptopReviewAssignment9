3df6237a936fd6e8e4e0b341402aeca87a18d693
codeftshaunak <sagardeyyash19@gmail.com>
2022-04-04 18:14:22 +0600

    review section done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-function Review() {
+function Review(props) {
+        console.log(props.review);
   return (
-    <div>Review</div>
+          <div>
+                  <img src={props.review.profile} alt="images" />
+                  <h4>Customer Name:{props.review.profile_name}</h4>       
+                  <h5>Product Name:{props.review.product}</h5>
+                  <h6>Review:{props.review.review_text}</h6>
+                  <h6>Ratings:{props.review.review_rating}</h6>
+          </div>
   )
 }
 
