aaf9471846b0175c8cce08e884afde502e115b46
codeftshaunak <sagardeyyash19@gmail.com>
2022-04-04 19:45:10 +0600

    Map items into home page

--- src/components/Carusel/Carusel.js
+++ src/components/Carusel/Carusel.js
@@ -4,13 +4,13 @@ import './Carusel.css';
 const Carusel=(props)=> {
   return (
                 <div className='carusel__Item'>
-                {/* <img src={props.review.profile} alt="images" />
+                <img src={props.review.profile} alt="images" />
                 <div className="titles">
                    <h4>Customer Name:{props.review.profile_name}</h4>       
                    <h5>Product Name:{props.review.product}</h5>
                    <h6>Review:{props.review.review_text}</h6>
                    <h6>Ratings:{props.review.review_rating}</h6>          
-                </div> */}
+                </div>
           </div>
   )
 }
