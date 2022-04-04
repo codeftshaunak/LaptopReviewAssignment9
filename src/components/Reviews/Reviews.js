436929d8b181e7627225a265358e116fcfa1b3f5
codeftshaunak <sagardeyyash19@gmail.com>
2022-04-04 17:54:53 +0600

    Json data loaded

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import Review from '../Review/Review';
 
 const Reviews = () => {
-  const [review, setReview] = useState({});
+  const [review, setReview] = useState([]);
   useEffect(() => {
     fetch('reviewsdata.json')
       .then(resp => resp.json())
-      .then(data => console.log(data))
+      .then(data => setReview(data))
   }, [])
   
   return (
     <div className='reviews'> 
-      
+      {review.map((review => <Review
+        key={review.id}
+        review={review}
+      ></Review>))}
     </div>
   )
 }
