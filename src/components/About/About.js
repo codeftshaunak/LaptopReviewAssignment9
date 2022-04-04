6c3e44c940ed0be5a6c4601948d815b82dba0d58
codeftshaunak <sagardeyyash19@gmail.com>
2022-04-04 22:08:37 +0600

    blog and about done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+        <h2>Laptop For Start <span>Codeing</span></h2>
+          <h2>Best <span>Laptop</span> For Starting </h2>
+          
+        <p>
+          Hey are you wanna start coding?
+          But you have low budget for starting your journy.
+          Don't worry we are here for help you. We have some collection that maybe match with you budget.
+          Happy learning.
+        </p>
+    </div>
   )
 }
 
